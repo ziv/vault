e3408a7b5a91353c21bc2646c6ca3e1bfe8a66f1
Ziv.Perry <ziv.perry@gmail.com>
2020-12-10 12:20:44 +0200

    add "empty" to store

--- src/vault.service.ts
+++ src/vault.service.ts
@@ -1,41 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { NameSpace, Vault, vaultCreatedKey } from '@xpr/vaultier';
+import {Injectable} from '@nestjs/common';
+import {NameSpace, Vault, vaultCreatedKey} from '@xpr/vaultier';
+import {vaultEmptyKey} from '../../vaultier/src';
 
 @Injectable()
 export class VaultService {
-  public storage = new Map<NameSpace, Vault>();
+    public storage = new Map<NameSpace, Vault>();
 
-  status(): any {
-    const status = [];
-    for (const [key, value] of this.storage.entries()) {
-      const created = value[vaultCreatedKey];
-      const count = Object.keys(value).length - 1;
-      status.push({ key, created, count });
+    status(): any {
+        const status = [];
+        for (const [key, value] of this.storage.entries()) {
+            const created = value[vaultCreatedKey];
+            const count = Object.keys(value).length - 1;
+            status.push({key, created, count});
+        }
+        return status;
     }
-    return status;
-  }
 
-  store(ns: NameSpace, vault: Vault) {
-    this.storage.set(ns, {
-      ...vault,
-      [vaultCreatedKey]: Date.now().toString(),
-    });
-    return true;
-  }
-
-  fetch(ns: NameSpace): Vault {
-    if (!this.storage.has(ns)) {
-      this.store(ns, {});
+    store(ns: NameSpace, vault: Vault) {
+        this.storage.set(ns, {
+            ...vault,
+            [vaultCreatedKey]: Date.now().toString(),
+        });
+        return true;
     }
-    return this.storage.get(ns);
-  }
 
-  flush(ns: NameSpace) {
-    this.storage.delete(ns);
-    return true;
-  }
+    fetch(ns: NameSpace): Vault {
+        if (!this.storage.has(ns)) {
+            this.store(ns, {[vaultEmptyKey]: '1'});
+        }
+        return this.storage.get(ns);
+    }
 
-  flushAll() {
-    this.storage = new Map<NameSpace, Vault>();
-  }
+    flush(ns: NameSpace) {
+        this.storage.delete(ns);
+        return true;
+    }
+
+    flushAll() {
+        this.storage = new Map<NameSpace, Vault>();
+    }
 }
